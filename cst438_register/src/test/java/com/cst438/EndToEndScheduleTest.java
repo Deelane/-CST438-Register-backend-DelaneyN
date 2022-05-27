8390a132316f84185d6ba415a565c3bd048ce47d
Deelane <Dnikoof@gmail.com>
2022-05-27 13:31:58 -0700

    fix imports

--- cst438_register/src/test/java/com/cst438/EndToEndScheduleTest.java
+++ cst438_register/src/test/java/com/cst438/EndToEndScheduleTest.java
@@ -1,7 +1,6 @@
 package com.cst438;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.concurrent.TimeUnit;
 
