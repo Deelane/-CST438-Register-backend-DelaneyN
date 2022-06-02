f07e6dff6f67a4047007de52cf13446f62b58c00
Deelane <Dnikoof@gmail.com>
2022-06-02 14:37:04 -0700

    Added Oauth2 functionality through google, updated schedule and student controllers. Created login controller, updated application properties, updated main

--- src/main/java/com/cst438/controller/ScheduleController.java
+++ src/main/java/com/cst438/controller/ScheduleController.java
@@ -5,14 +5,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -47,10 +49,12 @@ public class ScheduleController {
 	 * get current schedule for student.
 	 */
 	@GetMapping("/schedule")
-	public ScheduleDTO getSchedule( @RequestParam("year") int year, @RequestParam("semester") String semester ) {
-		System.out.println("/schedule called.");
-		String student_email = "test@csumb.edu";   // student's email 
+	public ScheduleDTO getSchedule( @RequestParam("year") int year, @RequestParam("semester") String semester, @AuthenticationPrincipal OAuth2User principal) {
 		
+		//retrieve email from the logged in user
+		String student_email = principal.getAttribute("email");   // student's email 
+		
+		//if the logged in user's email is in the student table, then they are a student
 		Student student = studentRepository.findByEmail(student_email);
 		if (student != null) {
 			System.out.println("/schedule student "+student.getName()+" "+student.getStudent_id());
@@ -67,9 +71,10 @@ public class ScheduleController {
 	
 	@PostMapping("/schedule")
 	@Transactional
-	public ScheduleDTO.CourseDTO addCourse( @RequestBody ScheduleDTO.CourseDTO courseDTO  ) { 
+	public ScheduleDTO.CourseDTO addCourse( @RequestBody ScheduleDTO.CourseDTO courseDTO, @AuthenticationPrincipal OAuth2User principal  ) { 
 
-		String student_email = "test@csumb.edu";   // student's email 
+		//retrieve email from the logged in user
+		String student_email = principal.getAttribute("email");   // student's email 
 		
 		Student student = studentRepository.findByEmail(student_email);
 		Course course  = courseRepository.findById(courseDTO.course_id).orElse(null);
@@ -99,9 +104,10 @@ public class ScheduleController {
 	
 	@DeleteMapping("/schedule/{enrollment_id}")
 	@Transactional
-	public void dropCourse(  @PathVariable int enrollment_id  ) {
+	public void dropCourse(  @PathVariable int enrollment_id, @AuthenticationPrincipal OAuth2User principal  ) {
 		
-		String student_email = "test@csumb.edu";   // student's email 
+		//retrieve email from the logged in user
+		String student_email = principal.getAttribute("email");   // student's email 
 		
 		// TODO  check that today's date is not past deadline to drop course.
 		
